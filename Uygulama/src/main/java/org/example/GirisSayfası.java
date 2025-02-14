5738c4c68b5f9770a6eeb22883b6a7cc6c08e0f9
bedirhan ergün <bedirhan1029@gmail.com>
2025-02-14 18:02:59 +0300

    kullanıcı adı textField eklendi

--- Uygulama/src/main/java/org/example/GirisSayfası.java
+++ Uygulama/src/main/java/org/example/GirisSayfası.java
@@ -6,11 +6,27 @@ import java.awt.*;
 public class GirisSayfası {
 
     JFrame frame;
+    JTextField kullaniciAdi;
+
     public GirisSayfası() {
+
+        JPanel panelUst = new JPanel();
+        panelUst.setLayout(new FlowLayout());
+        JLabel labelUst = new JLabel("Kullanıcı Adı:");
+        labelUst.setFont(new Font("Arial", Font.PLAIN, 20));
+
+        kullaniciAdi = new JTextField();
+        kullaniciAdi.setPreferredSize(new Dimension(200, 40));
+
+        panelUst.add(labelUst);
+        panelUst.add(kullaniciAdi);
+
         frame = new JFrame("Giriş Sayfası");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 300);
         frame.setLayout(new GridLayout(3,1));
+
+        frame.add(panelUst);
         frame.setVisible(true);
     }
 
