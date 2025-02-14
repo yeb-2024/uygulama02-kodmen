26bc87a049a66338562644fc430eac4ca94276db
bedirhan ergün <bedirhan1029@gmail.com>
2025-02-14 18:11:09 +0300

    Şifre için textField eklendi

--- Uygulama/src/main/java/org/example/GirisSayfası.java
+++ Uygulama/src/main/java/org/example/GirisSayfası.java
@@ -7,26 +7,42 @@ public class GirisSayfası {
 
     JFrame frame;
     JTextField kullaniciAdi;
+    JTextField sifre;
 
     public GirisSayfası() {
 
         JPanel panelUst = new JPanel();
-        panelUst.setLayout(new FlowLayout());
+        panelUst.setLayout(new FlowLayout(FlowLayout.RIGHT));
         JLabel labelUst = new JLabel("Kullanıcı Adı:");
         labelUst.setFont(new Font("Arial", Font.PLAIN, 20));
 
         kullaniciAdi = new JTextField();
-        kullaniciAdi.setPreferredSize(new Dimension(200, 40));
+        kullaniciAdi.setPreferredSize(new Dimension(300, 60));
 
         panelUst.add(labelUst);
         panelUst.add(kullaniciAdi);
 
+        JPanel panelAlt = new JPanel();
+        panelAlt.setLayout(new FlowLayout(FlowLayout.RIGHT));
+
+        JLabel labelAlt = new JLabel("Şifre: ");
+        labelAlt.setFont(new Font("Arial", Font.PLAIN, 20));
+
+        sifre = new JTextField();
+        sifre.setPreferredSize(new Dimension(300, 60));
+
+        panelAlt.add(labelAlt);
+        panelAlt.add(sifre);
+
+
         frame = new JFrame("Giriş Sayfası");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
+        frame.setSize(500, 400);
         frame.setLayout(new GridLayout(3,1));
 
         frame.add(panelUst);
+        frame.add(panelAlt);
+
         frame.setVisible(true);
     }
 
