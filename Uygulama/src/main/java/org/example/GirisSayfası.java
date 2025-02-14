2ae3d61a8edfb88d7ae2f86c2147383970f5bf9d
bedirhan ergün <bedirhan1029@gmail.com>
2025-02-14 18:39:35 +0300

    Button oluşturuldu ve boşmu kontrolu eklendi

--- Uygulama/src/main/java/org/example/GirisSayfası.java
+++ Uygulama/src/main/java/org/example/GirisSayfası.java
@@ -2,12 +2,15 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GirisSayfası {
+public class GirisSayfası implements ActionListener {
 
     JFrame frame;
     JTextField kullaniciAdi;
     JTextField sifre;
+    JButton button;
 
     public GirisSayfası() {
 
@@ -34,6 +37,10 @@ public class GirisSayfası {
         panelAlt.add(labelAlt);
         panelAlt.add(sifre);
 
+        button = new JButton("Giriş Yap");
+        button.setFont(new Font("Arial", Font.PLAIN, 20));
+        button.addActionListener(this);
+
 
         frame = new JFrame("Giriş Sayfası");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -42,8 +49,27 @@ public class GirisSayfası {
 
         frame.add(panelUst);
         frame.add(panelAlt);
+        frame.add(button);
 
         frame.setVisible(true);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button){
+            String kullaniciAdi = this.kullaniciAdi.getText().trim();
+            String sifre = this.sifre.getText().trim();
+
+            if (kullaniciAdi.isEmpty() || sifre.isEmpty() ){
+                JOptionPane.showMessageDialog(
+                        null,
+                        "Lütfen tüm alanları doldurun!",
+                        "Hata",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+
 
 }
